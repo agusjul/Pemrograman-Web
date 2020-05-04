6d1fe0af992181eff43bac5e7d0631b125e630fb
Bro Sand <bsand8813@gmail.com>
2020-05-04 18:02:38 +0800

    Penambahan php

---
+++ Pertemuan 9/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Instalasi PHP Primakara</title>
+</head>
+<body>
+    <h1> <?php echo "Today is " . date ("Y/m/d") . "<br>" ; ?></h1>
+
+    <?php echo '<h4>Nama : I Ketut Agus Juliana</h4>' ; ?>
+    <?php echo '<h4>NIM : 1801010043</h4>' ; ?>
+    <?php echo '<h4>Prodi : Sistem Informasi Pagi</h4>' ; ?>
+
+    <?php echo '<h3>Mencoba Belajar PHP untuk Pemrograman Web</h3>' ; ?>
+    <br>
+</body>
+</html>
