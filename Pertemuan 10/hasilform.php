9974a53f51d3eb22bec678c7167f27da0cb7da11
Bro Sand <bsand8813@gmail.com>
2020-05-20 15:39:43 +0800

    passing php variables

---
+++ Pertemuan 10/hasilform.php
@@ -0,0 +1,15 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hasil Form</title>
+</head>
+<body>
+    <h1>Selamat Datang <?php echo $_POST ["username"]; ?> </h1>
+    <br>
+    <h3>Password anda : <?php echo $_POST ["password"] ?></h3>
+</body>
+</html>
